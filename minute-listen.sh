a0dcee0b37988de2b1a85bbe259142e648d9d3eb
Your Name <you@example.com>
2025-11-16 18:24:57 -0700

    update

--- minute-listen.sh
+++ minute-listen.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
-# minute-listener.sh - Background listener to signal dwmblocks on minute changes
-# Run this as: minute-listener.sh &
-
-SIGNAL=1  # Match this to your block's update signal in config.h
+SIGNAL=1
 
 while true; do
-    # Current time in seconds with nanoseconds
     now=$(date +%s.%N)
-    
-    # Compute seconds until next full minute
-    # % 60 gives the seconds part, subtract from 60 to get remaining
-    sec_until_next=$(
-        echo "60 - ($now % 60)" | bc -l
-    )
+    sec_int=${now%.*}
+    sec_frac=${now#*.}
+    sec_frac="0.$sec_frac"
 
-    # Sleep that exact amount
+    # Seconds until the next minute
+    sec_until_next=$(awk "BEGIN{print 60 - ($sec_int % 60) - $sec_frac}")
+
     sleep "$sec_until_next"
-
-    # Signal dwmblocks
     pkill -RTMIN+"$SIGNAL" dwmblocks
 done
+
