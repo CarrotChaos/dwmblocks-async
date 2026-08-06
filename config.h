6ebafa2693470e0f95d8d27c7908d86da4a3e889
Your Name <you@example.com>
2026-08-06 13:09:41 -0600

    update

--- config.h
+++ config.h
@@ -17,12 +17,11 @@
 #define TRAILING_DELIMITER 0
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
-#define BLOCKS(X)                                  \
-    X("", "~/scripts/statusbar/audio.sh", 0, 10)   \
+#define BLOCKS(X)                                 \
+    X("", "~/scripts/statusbar/audio.sh", 0, 10)  \
     X("", "~/scripts/statusbar/battery.sh", 0, 6) \
-    X("", "~/scripts/statusbar/cpu.sh", 5, 7)      \
-    X("", "~/scripts/statusbar/ram.sh", 5, 8)      \
-    X("", "~/scripts/statusbar/wifi.sh", 30, 2)      \
+    X("", "~/scripts/statusbar/cpu.sh", 5, 7)     \
+    X("", "~/scripts/statusbar/ram.sh", 5, 8)     \
     X("", "~/scripts/statusbar/date.sh", 0, 1)
 
 #endif  // CONFIG_H
